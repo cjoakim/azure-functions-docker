cbdc1c08933edd65aef34910918d783a942eaeff
Chris Joakim <christopher.joakim@gmail.com>
2020-05-12 18:06:48 -0400

    PyHttp1/__init__.py - context, fstrings, and return json

--- PythonFunctionsProject/PyHttp1/__init__.py
+++ PythonFunctionsProject/PyHttp1/__init__.py
@@ -1,13 +1,26 @@
-import logging
-import azure.functions as func
+import json
+import logging as log
+import os
+
 import arrow
+import azure.functions as func
+
+
 
 from ..shared_code import common
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('PyHttp1 HTTP trigger; app_version: {}'.format(common.app_version()))
+def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    fname  = context.function_name
+    inv_id = context.invocation_id
+    log.info(f'{fname} HTTP trigger, invocation_id: {inv_id}, app_version: {common.app_version()}')
+
+    resp_doc = dict()
+    resp_doc['function_name'] = fname
+    resp_doc['invocation_id'] = inv_id
+    resp_doc['runtime'] = os.environ["FUNCTIONS_WORKER_RUNTIME"]
 
     name = req.params.get('name')
+
     if not name:
         try:
             req_body = req.get_json()
@@ -19,10 +32,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     if name:
         now   = common.now()
         epoch = common.epoch()
-        msg   = 'Hello {}, the date time is {}, epoch {} (common)'.format(name, str(now), epoch)
-        logging.info(msg)
-        return func.HttpResponse(msg)
+        resp_doc['msg'] = f'Hello {name}, the date time is {str(now)}, epoch {epoch}'
+        log.info(json.dumps(resp_doc))
+        return func.HttpResponse(json.dumps(resp_doc, indent=4, sort_keys=False))
     else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400)
+        resp_doc['error_msg'] = 'Please pass a name on the query string or in the request body'
+        return func.HttpResponse(json.dumps(resp_doc, indent=4, sort_keys=False), status_code=400)
