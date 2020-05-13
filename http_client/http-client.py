f526d48b41c68761b89a050472efdb2c0c4a897d
Chris Joakim <christopher.joakim@gmail.com>
2020-05-13 18:05:40 -0400

    http-client.py geo queries implemented for local and azure

--- http_client/http-client.py
+++ http_client/http-client.py
@@ -4,7 +4,8 @@ Usage:
   python http-client.py load_regions local
   python http-client.py query_by_pk local eastus
   python http-client.py query_by_pk local germanynorth
-  python http-client.py query_by_geo local 8.806422 53.073635  10
+  python http-client.py query_by_geo local 8.806422 53.073635 10
+  python http-client.py query_by_geo azure -78.3889, 36.6681 200
 """
 
 # Python HTTP Client program for this Azure Function.
